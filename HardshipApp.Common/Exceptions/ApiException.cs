72bd7686efa356316273213601691b4b4ec956a9
Yuntannn <jonathan2014max@gmail.com>
2026-04-04 21:22:48 +1100

    feat(HA-5): add global exception handler nad custom exceptions

---
+++ HardshipApp.Common/Exceptions/ApiException.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Http;
+namespace HardshipApp.Common.Exceptions;
+public class ApiException : Exception
+{
+    public int StatusCode {get; set;}
+    public IDictionary<string, string[]>? Errors {get; set;}
+    public ApiException(string message,
+                        int statusCode,
+                        IDictionary<string, string[]>? errors = null,
+                        Exception? innerException = null) : base(message, innerException)
+    {
+        StatusCode = statusCode;
+        Errors = errors ?? new Dictionary<string, string[]>();
+    }  
+}
+public sealed class UnauthorizedException : ApiException
+{
+    public UnauthorizedException(string message = "Unauthorized") : base(message, StatusCodes.Status401Unauthorized){ }
+}
+public sealed class  NotFoundException : ApiException
+{
+    public NotFoundException(string message = "Resource not found") : base(message, StatusCodes.Status404NotFound){ }
+}
+public sealed class ConflictException : ApiException
+{
+    public ConflictException(string message = "A conflict occured") : base(message, StatusCodes.Status409Conflict){ }
+}
